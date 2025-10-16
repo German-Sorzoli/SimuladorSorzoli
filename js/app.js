a99826037df6320c9803f72ccad8134737a484f9
GermanSorzoli <glsorzoli@gmail.com>
2025-10-16 00:23:31 -0300

    Revision Funciones y Eventos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-// Definicion de variable globales, constantes y arrays
+// *************************** VARIABLES ********************************* //
 
-let datosUsuario = [];
+// Creo un array de objetos para los productos de la web
 const listaProductos = [
     {id:1, nombre:"Nintendo Switch 2", precio:350},
     {id:2, nombre:"Play Station 5", precio:460}, 
@@ -12,49 +12,62 @@ const listaProductos = [
     {id:8, nombre:"Nintendo Switch OLED", precio:300},
     {id:9, nombre:"XBox 360", precio:100},
     ];
-const precioDolar = 1400;
 
-let carritoUsuario = [] || JSON.parse(localStorage.getItem("CarritoTG")); // Si existe un carrito en el localStorage, lo cargo, sino inicializo un array vacio
+// Defino un array de objetos que contendrá los productos agregados al carrito.
+// Si ya hay informacion en el navegador, tomara esos datos, sino se iniciará vacío.
+let carritoUsuario = [] || JSON.parse(localStorage.getItem("CarritoTG"));
+// Variable global que almacena el total del carrito
 let totalCompra;
-let listadoCompra;
 
-// Definicion de funciones
+// *************************** FUNCIONES ********************************* //
 
-// Vuelco productos en HTML
+// Funciones para agregar o sacar un producto al carrito, reciben un objeto con los datos del producto.
+// Esto agregará una nueva propiedad de cantidad de productos y subtotal por cada producto.
 
-let productos = document.querySelector('.listaProductos');
-// Recorro el array de productos
- listaProductos.forEach((producto) =>{
-    let article = document.createElement("article");
-    article.innerHTML = `
-    <h4>${producto.nombre}</h4>
-    <p>USD ${producto.precio}.-</p>
-    <button class='agregarAlCarro operacionAgregar' data-id=${producto.id}>Agregar al carrito</button>
-    `;
-    // Agrego el article al section de productos
-    productos.appendChild(article);
-});
+// Si el producto no existia previamente en el carrito, lo agrega.
+function iniciarProducto(producto){
+    producto.cantidad = 1;
+    producto.subtotal = producto.precio * producto.cantidad;
+    carritoUsuario.push(producto); 
+    localStorage.setItem("CarritoTG", JSON.stringify(carritoUsuario));  
+}
 
+// Si el producto ya existe en el carrito, incrementa la cantidad y recalcula subtotal
+function agregarProducto(producto){
+    producto.cantidad++;
+    producto.subtotal = producto.precio * producto.cantidad;
+    localStorage.setItem("CarritoTG", JSON.stringify(carritoUsuario));  
+}
 
-// Agrego funcionalidad a los botones de agregar al carrito.
+// Eliminar el producto, siempre y cuando no sea menor a 0
+function sacarProducto(producto){
+    if(producto.cantidad>0)
+        {producto.cantidad--;
+        producto.subtotal = producto.precio * producto.cantidad;
+        console.log(producto);
+        localStorage.setItem("CarritoTG", JSON.stringify(carritoUsuario));  
+        }
+    else {
+        delete carritoUsuario[producto.id];
+        console.log(`El producto ${producto.nombre} fue eliminado`);
+        localStorage.setItem("CarritoTG", JSON.stringify(carritoUsuario));
+    }
+}
 
-let botonesAgregar = document.querySelectorAll('.operacionAgregar');
-botonesAgregar.forEach((boton) => {
-    // Detecto evento click en cada boton
-    boton.addEventListener('click', (e) => {
-        // Dentro de productoSeleccionado guardare la informacion del producto clickeado
-        // Comparo el id del producto con el id del boton clickeado
-        let productoSeleccionado = listaProductos.find((producto) => producto.id == e.target.dataset.id); 
-        // Verifico si el producto ya se encuentra en el carrito
-        if (carritoUsuario.find((producto) => producto.id == productoSeleccionado.id)){
-            // Si ya existe, aumento la cantidad de productos en el carrito
-            agregarProducto(productoSeleccionado);}
-            else {
-            // Si no existe, lo agrego al carrito con cantidad 1
-            iniciarProducto(productoSeleccionado);
-            }
-    });
-});
+// Funcion que elimina todos los productos del carrito
+function vaciarCarrito(){
+    carritoUsuario = [];
+    totalCompra = 0;
+    carritoVacio();
+}
+// Funcion que muestra un mensaje si el carrito esta vacio
+
+function carritoVacio(){
+let mensajeVacio = document.querySelector('.listaCarrito');
+    mensajeVacio.innerHTML = `<h4>No tenés productos en tu carrito...</h4>
+    <p><br>Volvé a nuestra sección de productos y conocé nuestras ofertas</P>`;
+    localStorage.removeItem("CarritoTG");
+}
 
 // Funcion para renderizar un listado del carrito
 
@@ -93,112 +106,6 @@ retomarCarrito();
 calcularTotal();
 }
 
-// Alternar entre vista carrito y productos
-let carritoYProductos = document.querySelector('.verCarrito');
-let articulosProductos = document.querySelector('.listaProductos');
-let productosEnCarrito = document.querySelector('.listaCarrito');
-let infoSeccion = document.querySelector('h3');
-
-carritoYProductos.addEventListener('click', () => {
-    // Agrego clase con estilo del boton
-    if (carritoYProductos.innerText == "Ver carrito"){
-        // Si se encuentra en modo productos, muestra el carrito
-        carritoYProductos.innerText = "Volver a Productos";
-        carritoYProductos.classList.add('volverHome');
-        // Oculto productos y muestro carrito
-        articulosProductos.classList.add('hidden');
-        productosEnCarrito.classList.remove('hidden');
-        infoSeccion.innerText = "Estos son los productos en tu carrito:";
-        mostrarCarrito();
-    }
-    // Si se encuentra en modo carrito, vuelve a modo productos
-    else if(carritoYProductos.innerText == "Volver a Productos"){
-        carritoYProductos.innerText = "Ver carrito";
-        carritoYProductos.classList.remove('volverHome');
-        // Oculto carrito y muestro productos
-        articulosProductos.classList.remove('hidden');
-        productosEnCarrito.classList.add('hidden');
-        infoSeccion.innerText = "Elige entre nuestros productos";
-    }
-})
-
-
-// Funcion para calcular el total de la compra
-function calcularTotal (){
-    totalCompra = 0;
-    let productosEnCarrito = document.querySelector('.listaCarrito');
-    
-    let total = document.createElement("article");
-    carritoUsuario.forEach((producto) =>{
-        totalCompra = totalCompra + producto.subtotal;
-    });
-    total.innerHTML = `<p>TOTAL</p>
-    <p id=#total>USD ${totalCompra}.-</p>`
-    total.classList.add("totalCarrito");
-    productosEnCarrito.appendChild(total);
-
-    // Agrego botones de vaciar carrito
-    let botonVaciar = document.createElement("button");
-    botonVaciar.innerHTML = `VACIAR`;
-    botonVaciar.classList.add("botones");
-    botonVaciar.classList.add("operacionVaciar");
-    botonVaciar.addEventListener('click', () => {
-        console.log('Se vacio el carrito');
-        vaciarCarrito();})
-    productosEnCarrito.appendChild(botonVaciar);
-
-    // Agrego boton de finalizar la compra
-    let botonFinalizar = document.createElement("button");
-    botonFinalizar.innerHTML = `FINALIZAR COMPRA`;
-    botonFinalizar.classList.add("botones");
-    botonFinalizar.classList.add("operacionFinalizar");
-    botonFinalizar.addEventListener('click', () => {
-        console.log('Se concretó la compra');
-        finalizarCarrito();})
-    productosEnCarrito.appendChild(botonFinalizar);
-
-};
-
-// funcion agregar producto al carrito. Esto agregara una nueva propiedad de cantidad de productos y subtotal en el carrito por cada objeto.
-
-function agregarProducto(producto){
-    producto.cantidad++;
-    producto.subtotal = producto.precio * producto.cantidad;
-    console.log(producto);
-    localStorage.setItem("CarritoTG", JSON.stringify(carritoUsuario));  
-}
-
-function iniciarProducto(producto){
-    producto.cantidad = 1;
-    producto.subtotal = producto.precio * producto.cantidad;
-    carritoUsuario.push(producto); 
-    console.log(producto);
-    localStorage.setItem("CarritoTG", JSON.stringify(carritoUsuario));  
-}
-
-function sacarProducto(producto){
-    if(producto.cantidad>0)
-        {producto.cantidad--;
-        producto.subtotal = producto.precio * producto.cantidad;
-        console.log(producto);
-        localStorage.setItem("CarritoTG", JSON.stringify(carritoUsuario));  
-        }
-    else {
-        delete carritoUsuario[producto.id];
-        console.log(`El producto ${producto.nombre} fue eliminado`);
-        localStorage.setItem("CarritoTG", JSON.stringify(carritoUsuario));
-    }
-}
-
-function vaciarCarrito(){
-    let productosEnCarrito = document.querySelector('.listaCarrito');
-    carritoUsuario = [];
-    console.log(carritoUsuario);
-    productosEnCarrito.innerHTML = `<h4 class="noItems">No tenés productos en tu carrito...</h4>
-    <p><br>Volvé a nuestra sección de productos y conocé nuestras ofertas</P>`;
-    localStorage.removeItem("CarritoTG");
-    totalCompra = 0;
-}
 
 function retomarCarrito (){
     let botonesCarritoAgregar = document.querySelectorAll('.agregarUnProducto');
@@ -221,24 +128,139 @@ function retomarCarrito (){
             recalcularTotal();
         })
     });
-
+    
     botonesCarritoRestar.forEach((boton) => {
     // Detecto evento click en cada boton
     boton.addEventListener('click', (e) => {
         // Dentro de productoSeleccionado guardare la informacion del producto clickeado
         // Comparo el id del producto con el id del boton clickeado
-        let productoSeleccionado = listaProductos.find((producto) => producto.id == e.target.dataset.id);
+        let idProducto = parseInt(e.target.dataset.id);        
+        let productoSeleccionado = carritoUsuario.find((producto) => producto.id == idProducto);
             // Resto la cantidad de productos en el carrito
             sacarProducto(productoSeleccionado);
+            // Actualizo el HTML
+            let cantidadElemento = document.querySelector(`.cantidad-id-${idProducto}`);
+            cantidadElemento.textContent = productoSeleccionado.cantidad;
+            let subtotalElemento = cantidadElemento.parentElement.nextElementSibling;
+            subtotalElemento.textContent = `USD ${productoSeleccionado.subtotal}.-`;
+            recalcularTotal();
         })})
 };
 
+//
 function recalcularTotal() {
     let totalElemento=document.querySelector(`#total`);
+    console.log(totalElemento);
     totalCompra = 0;
     carritoUsuario.forEach((producto) =>{
         totalCompra = totalCompra + producto.subtotal;
     });
-    total.textContent = `USD ${totalCompra}.-`;
+    
+    totalElemento.textContent = `USD ${totalCompra}.-`;
     localStorage.setItem("CarritoTG", JSON.stringify(carritoUsuario));
 }
+
+// Funcion para calcular el total de la compra
+function calcularTotal (){
+    totalCompra = 0;
+    let productosEnCarrito = document.querySelector('.listaCarrito');
+    
+    let total = document.createElement("article");
+    carritoUsuario.forEach((producto) =>{
+        totalCompra = totalCompra + producto.subtotal;
+    });
+    total.innerHTML = `<p>TOTAL</p>
+    <p id=total>USD ${totalCompra}.-</p>`
+    total.classList.add("totalCarrito");
+    productosEnCarrito.appendChild(total);
+
+    // Agrego botones de vaciar carrito
+    let botonVaciar = document.createElement("button");
+    botonVaciar.innerHTML = `VACIAR`;
+    botonVaciar.classList.add("botones");
+    botonVaciar.classList.add("operacionVaciar");
+    botonVaciar.addEventListener('click', () => {
+        vaciarCarrito();})
+    productosEnCarrito.appendChild(botonVaciar);
+
+    // Agrego boton de finalizar la compra
+    let botonFinalizar = document.createElement("button");
+    botonFinalizar.innerHTML = `FINALIZAR COMPRA`;
+    botonFinalizar.classList.add("botones");
+    botonFinalizar.classList.add("operacionFinalizar");
+    botonFinalizar.addEventListener('click', () => {
+        console.log('Se concretó la compra');
+        finalizarCarrito();})
+    productosEnCarrito.appendChild(botonFinalizar);
+};
+
+
+// *************************** EVENTOS ********************************* //
+
+// Tomo los productos dentro de listaProductos y los agrego al HTML
+let productos = document.querySelector('.listaProductos');
+// Recorro el array de productos
+ listaProductos.forEach((producto) =>{
+    let articleProducto = document.createElement("article");
+    articleProducto.innerHTML = `
+    <h4>${producto.nombre}</h4>
+    <p>USD ${producto.precio}.-</p>
+    <button class='agregarAlCarro operacionAgregar' data-id=${producto.id}>Agregar al carrito</button>
+    `;
+    // Agrego el article al section de listaProductos en el HTML
+    productos.appendChild(articleProducto);
+});
+
+// Agrego funcionalidad a los botones de agregar al carrito en la pagina principal.
+let botonesAgregar = document.querySelectorAll('.operacionAgregar');
+botonesAgregar.forEach((boton) => {
+    // Detecto evento click en cada boton.
+    boton.addEventListener('click', (e) => {
+        // Dentro de productoSeleccionado guardaré la informacion del producto clickeado.
+        // Comparo el id del producto con el id del boton clickeado.
+        const productoSeleccionado = listaProductos.find((producto) => producto.id == e.target.dataset.id); 
+        // Verifico si el producto ya se encuentra en el carrito
+        if (carritoUsuario.find((producto) => producto.id == productoSeleccionado.id)){
+            // Si ya existe, aumento la cantidad de productos en el carrito.
+            agregarProducto(productoSeleccionado);}
+            else {
+            // Si no existe, lo agrego al carrito con cantidad 1.
+            iniciarProducto(productoSeleccionado);
+            }
+    });
+});
+
+// Alternar entre vista carrito/productos.
+
+// Variable que almacena el texto del boton, ya sea "Ver Carrito" o "Volver a Productos".
+let carritoYProductos = document.querySelector('.verCarrito');
+// Variable que almacena listado de productos en HTML.
+let articulosProductos = document.querySelector('.listaProductos');
+// Variable que almacena listado del carrito en HTML.
+let productosEnCarrito = document.querySelector('.listaCarrito');
+// Variable para texto contextual de la sección.
+let infoSeccion = document.querySelector('h3');
+
+carritoYProductos.addEventListener('click', () => {
+    // Agrego clase con estilo del boton
+    if (carritoYProductos.innerText == "Ver carrito"){
+        // Si se encuentra en modo productos, muestra el carrito
+        carritoYProductos.innerText = "Volver a Productos";
+        carritoYProductos.classList.add('volverHome');
+        // Oculto productos y muestro carrito
+        articulosProductos.classList.add('hidden');
+        productosEnCarrito.classList.remove('hidden');
+        infoSeccion.innerText = "Estos son los productos en tu carrito:";
+        // Ejecuto la funcion para mostrar el carrito con los productos.
+        mostrarCarrito();
+    }
+    // Si se encuentra en modo carrito, vuelve a modo productos
+    else if(carritoYProductos.innerText == "Volver a Productos"){
+        carritoYProductos.innerText = "Ver carrito";
+        carritoYProductos.classList.remove('volverHome');
+        // Oculto carrito y muestro productos
+        articulosProductos.classList.remove('hidden');
+        productosEnCarrito.classList.add('hidden');
+        infoSeccion.innerText = "Elige entre nuestros productos";
+    }
+})
